ba8a80cbbb5651fe2167975f54fdbfb1afc1f0b6
user <juyelr16@gmail.com>
2022-02-17 23:35:29 +0900

    middel commit

--- js/moneySaving.js
+++ js/moneySaving.js
@@ -1,14 +1,45 @@
 document.getElementById('calc-btn').addEventListener('click', function () {
     const foodInput = document.getElementById('food-input')
-    const foodAmount = foodInput.value;
-    //get total-food expenses
+    const foodAmountText = foodInput.value;
+    const foodAmount = parseFloat(foodAmountText);
+
+    //rent cost
+    const rentInput = document.getElementById('rent-input')
+    const rentAmountText = rentInput.value;
+    const rentAmount = parseFloat(rentAmountText);
+
+    //cloth cost
+    const clothInput = document.getElementById('cloth-input')
+    const clothAmountText = clothInput.value;
+    const clothAmount = parseFloat(clothAmountText);
+    //get total expenses
     const expensesTotal = document.getElementById('total-expenses');
     const expensesTotalText = expensesTotal.innerText;
-    expensesTotal.innerText = foodAmount;
+    const previousExpensesTotal = parseFloat(expensesTotalText);
+    expensesTotal.innerText = foodAmount + rentAmount + clothAmount;
 
+    // get income 
+    const incomeInput = document.getElementById('income-input')
+    const incomeAmountText = incomeInput.value;
+    const incomeAmount = parseFloat(incomeAmountText);
+    // //get total expenses
+    // const expensesInput = document.getElementById('total-expenses');
+    // const expensesAmountText = expensesInput.value;
+    // const expensesAmount = parseFloat(expensesAmountText);
+    //get current blance
+    const balanceTotal = document.getElementById('balance-update');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = incomeAmount - (foodAmount + rentAmount + clothAmount);
 
     //reset input field
+    incomeInput.value = '';
+    foodInput.value = '';
+    rentInput.value = '';
+    clothInput.value = '';
 
 
-    console.log(expensesTotalText);
-})
+
+});
+
